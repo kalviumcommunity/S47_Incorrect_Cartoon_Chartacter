84ab209289fa3379bac3806a3fc015312b0288f5
IshitaNaraniya <ishita.n@kalvium.community>
2024-02-13 22:43:11 +0530

    Added Login Functionality using JWT

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+require('dotenv').config()
+
 const cors = require('cors')
 const express = require('express');
 const mongoose = require('mongoose')
+
 const app = express();
 const port = 3000;
-app.use(cors())
-app.use(express.json());
+const bodyParser = require('body-parser');
 const Modal = require('./Scheme');
 const { validateInput } = require('./validator');
+const jwt = require('jsonwebtoken')
+
+app.use(bodyParser.json());
+app.use(cors())
+app.use(express.json());
+
+const users = require('./users.json')
 
 mongoose.connect('mongodb+srv://ishita_naraniya:ishhMongodb@cluster1.ponxmip.mongodb.net/ASAP', {
   dbName: "ASAP"
@@ -19,20 +28,6 @@ app.get('/data', (req, res) => {
     .catch(err => res.json(err))
 })
 
-// app.post('/insert', (req, res) => {
-//   Modal.create(req.body)
-//     .then(users => res.json(users))
-//     .catch(err => console.log(err))
-
-//     const { error, value } = validateInput(req.body)
-
-//     if (error) {
-//       console.log(error)
-//       return res.send(error.details)
-//     }
-//     res.send('Successfully Added item!')
-  
-// })
 app.post('/insert', (req, res) => {
   const { error, value } = validateInput(req.body);
 
@@ -77,6 +72,21 @@ app.delete('/delete/:id', (req, res) => {
 app.get('/ping', (req, res) => {
   res.send('Hello World')
 })
+
+const SECRET_KEY = '1733060872e4a77a956a920d83de08b3a4e51cc1ecc0e1ba35f3c73df5ed9a2cc9467b2248cec1fad302697d4546aff441c5d21e05ecdd67ce096f0d27e8e5fa';
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).send({ message: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ sub: user.id }, SECRET_KEY, { expiresIn: '1h' });
+  res.send({ token });
+});
 
 
 app.listen(port, () => {
